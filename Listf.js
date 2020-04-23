60795fda477898a3c7b43b60d5483a61a29f7ba9
JokerkidEli <62958270+JokerkidEli@users.noreply.github.com>
2020-04-23 16:19:03 -0400

    Create Listf.js

---
+++ Listf.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  Keyboard,
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+  Alert,
+  KeyboardAvoidingView,
+  Button,
+  Image,
+  TouchableHighlight,
+  ImageBackground,Platform,Animated
+} from 'react-native';
+
+
+
+class Listf extends React.Component {
+
+  
+
+  render() {
+    return (
+      <View style={styles.container}>
+       
+        
+ 
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+  },
+
+});
+
+
+export default Listf;
