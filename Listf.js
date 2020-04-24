98168885c4dcf54f1f39e8b05bd74fcb7c1c0659
JokerkidEli <62958270+JokerkidEli@users.noreply.github.com>
2020-04-24 18:04:55 -0400

    Update Listf.js

--- Listf.js
+++ Listf.js
@@ -22,11 +22,45 @@ class Listf extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-       
-        
- 
 
+
+      <View style={styles.container}>
+       <View style={styles.textContainer}>
+            
+            <View style={styles.header}>
+                <Text style={styles.headerText}>
+                      Fruits
+                  </Text>
+            </View>
+            </View>
+      
+      <View style={styles.listContainer}>
+        
+        <Text style={styles.item}>
+        Blackberries
+        </Text>
+
+        <TouchableHighlight
+        onPress={() => this.props.navigation.navigate('')}
+        >
+        <Image source ={{uri: 'https://p2.piqsels.com/preview/92/728/1006/blackberries-on-a-plate-black-fruit.jpg'}}
+ style={{ height: 150, width: 150, justifyContent: 'center', }}
+        />
+        </TouchableHighlight>
+
+        <Text style={styles.item}>
+        Bananas
+        </Text>
+
+        <TouchableHighlight
+        onPress={() => this.props.navigation.navigate('')}
+        >
+        <Image source ={{uri: 'https://p0.pikist.com/photos/931/884/bananas-fruit-food-fresh-mature-yellow-vitamins-diet-thumbnail.jpg'}}
+ style={{ height: 150, width: 150, justifyContent: 'center', }}
+        />
+        </TouchableHighlight>
+
+    </View>
       </View>
     );
   }
@@ -37,7 +71,35 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+
+  listContainer:{
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  header:{
+    backgroundColor: 'lightgreen',
+        height: 40,
+        width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 4,
+  },
+
+  headerText:{
+    color: 'white',
+    size: 38,
+    fontWeight:'bold',
+  },
+
+  item:{
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 19,
+  },
+
 
 });
 
