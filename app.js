16012517cd76532cfc5a1b8a347132f90bf8daeb
JokerkidEli <62958270+JokerkidEli@users.noreply.github.com>
2020-05-17 00:41:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ import Menu from './src/login/menu.js';
 import Listf from './src/Fruit Category/Listf.js';
 import Listm from './src/Meat Category/Listm.js';
 import Lists from './src/Salad Category/Lists.js';
-
-
+import Banana from './src/Fruit Category/Banana.js';
+import Blackberries from './src/Fruit Category/Blackberries.js';
+import GrilledChicken from './src/Salad Category/GrilledChikSal.js';
+import SpinachSalad from './src/Salad Category/Spinach.js';
+import Turkey from './src/Meat Category/Turkey.js';
+import Pork from './src/Meat Category/Pork.js';
 
 
 const RootStack = createStackNavigator(
@@ -19,7 +23,12 @@ const RootStack = createStackNavigator(
   Listf:{ screen: Listf},
   Listm:{ screen: Listm},
   Lists:{ screen: Lists},
-  
+  Banana:{ screen: Banana },
+  Blackberries:{ screen: Blackberries },
+  GrilledChicken:{ screen: GrilledChicken },
+  SpinachSalad:{ screen: SpinachSalad },
+  Turkey:{ screen: Turkey },
+  Pork:{ screen: Pork },
 },
 {
     initialRouteName: 'Home',
@@ -32,4 +41,3 @@ export default class App extends Component {
     return <RootStack />;
   }
 }
-
